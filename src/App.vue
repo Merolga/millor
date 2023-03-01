eeee09da79db094243d5580a6b9007ff8ac3f58c
Olga <oolgaa75@mail.ru>
2023-03-01 19:50:35 +0300

    сохраню тут на время

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="millor">
+    {{ count }}
+    <hr>
+    <div style="display: flex; gap: 10px">
+      <TestComponent 
+        v-for="(tab, index) in tabs"
+        :key="`tab-${index}`"
+        :tab="tab"
+        @get-id="increaseCount"
+     />
+    </div>
     <HeaderMillor />
     <MainSlider />
     <ListCatalogs />
@@ -9,11 +19,13 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import HeaderMillor from './components/sections/HeaderMillor.vue';
 import MainSlider from './components/sections/MainSlider.vue';
 import ListCatalogs from './components/sections/ListCatalogs.vue';
 import ListDiscount from './components/sections/ListDiscount.vue';
 import OurAdvantage from './components/sections/OurAdvantage.vue';
+import TestComponent from './components/sections/TestComponent.vue';
 
 export default {
   name: 'App',
@@ -23,6 +35,39 @@ export default {
     ListCatalogs,
     ListDiscount,
     OurAdvantage,
+    TestComponent,
+  },
+  setup() {
+    const tabs = [
+      {
+        id: 1,
+        name: 'name 1',
+      },
+      {
+        id: 2,
+        name: 'name 2',
+      },
+      {
+        id: 3,
+        name: 'name 3',
+      },
+      {
+        id: 4,
+        name: 'name 4',
+      },
+    ];
+
+    const count = ref(0);
+
+    function increaseCount(n) {
+      count.value = n
+    }
+
+    return {
+        tabs,
+        increaseCount,
+        count,
+      };
   },
 }
 </script>
---
+++ src/components/sections/TestComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+      @click="$emit('get-id', tab.id)"
+  >
+    {{ tab.name }}
+  </button>  
+</template>
+
+<script>
+
+export default {
+  name: "TestComponent",
+  props: {
+    tab: {
+      type: Object,
+    }
+  },
+  setup() {
+    
+    return {
+
+    };
+  },
+};
+</script>
+
+<styles scoped lang="scss">
+</styles>
